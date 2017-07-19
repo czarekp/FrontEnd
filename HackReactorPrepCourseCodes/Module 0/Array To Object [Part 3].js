099f78a1bacefb6a2301cfe979d98e24c6612b5a
Unknown <cezarypiotrpietrzak@gmail.com>
2017-07-19 12:18:06 +0200

    Added solution for Module 0: Array To Object [Part 3]
    solution + instructions

---
+++ HackReactorPrepCourseCodes/Module 0/Array To Object [Part 3].js
@@ -0,0 +1,43 @@
+/*
+Instructions:
+Write a function called "transformEmployeeData" that transforms some employee data from one format to another.
+
+The argument will look something like this:
+[
+    [
+        ['firstName', 'Joe'], ['lastName', 'Blow'], ['age', 42], ['role', 'clerk']
+    ],
+    [
+        ['firstName', 'Mary'], ['lastName', 'Jenkins'], ['age', 36], ['role', 'manager']
+    ]
+]
+
+Given that input, the return value should look like this:
+[
+    {firstName: 'Joe', lastName: 'Blow', age: 42, role: 'clerk'},
+    {firstName: 'Mary', lastName: 'Jenkins', age: 36, role: 'manager'}
+]
+
+Note that the input may have a different number of rows or different keys than the given sample. 
+
+For example, let's say the HR department adds a "tshirtSize" field to each employee record. Your code should flexibly accommodate that.
+
+Starter Code :
+function transformEmployeeData(array) {
+  // your code here
+}
+*/
+
+function transformEmployeeData(array) {
+	let objArray = [];
+
+	for (let i = 0; i < array.length; i++) {
+		let obj = {};
+		for (let j = 0; j < array[i].length; j++) {
+			obj[array[i][j][0]] = array[i][j][1];
+		}
+		objArray[i] = obj;
+	}
+
+	return objArray;
+}
