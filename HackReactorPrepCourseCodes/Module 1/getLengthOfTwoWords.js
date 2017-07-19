d410a9e8dd9a4682b2a473ccdb7b00c22d13682b
Unknown <cezarypiotrpietrzak@gmail.com>
2017-07-19 12:55:41 +0200

    Added 4 solutions for Module 1

---
+++ HackReactorPrepCourseCodes/Module 1/getLengthOfTwoWords.js
@@ -0,0 +1,18 @@
+/*
+Instructions:
+Write a function called "getLengthOfTwoWords".
+
+Given 2 words, "getLengthOfTwoWords" returns the sum of their lengths.
+
+var output = getLengthOfTwoWords('some', 'words');
+console.log(output); // --> 9
+
+Starter Code :
+function getLengthOfTwoWords(word1, word2) {
+  // your code here
+}
+*/
+
+function getLengthOfTwoWords(word1, word2) {
+	return word1.length + word2.length;
+}
---
+++ HackReactorPrepCourseCodes/Module 1/getLengthOfWord.js
@@ -0,0 +1,18 @@
+/*
+Instructions:
+Write a function called "getLengthOfWord".
+
+Given a word, "getLengthOfWord" returns the length of the given word.
+
+var output = getLengthOfWord('some');
+console.log(output); // --> 4
+
+Starter Code :
+function getLengthOfWord(word) {
+  // your code here
+}
+*/
+
+function getLengthOfWord(word) {
+	return word.length;
+}
