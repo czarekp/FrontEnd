9973386700153a71acb1c919bc3b86ab66de47f7
Unknown <cezarypiotrpietrzak@gmail.com>
2017-07-20 00:43:48 +0200

    Few changes for code clarity and proper work of binary clock
    + changed getHours and getMinutes to getUTChours and getUTCminutes
    + implemented conversion for UTC+02:00
    + changed double quotation marks to single for code to be clear and consistent

--- My Projects/binaryClock/script.js
+++ My Projects/binaryClock/script.js
@@ -1,94 +1,94 @@
-const firstClockColumn = document.querySelectorAll(".first");
-const secondClockColumn = document.querySelectorAll(".second");
-const thirdClockColumn = document.querySelectorAll(".third");
-const fourthClockColumn = document.querySelectorAll(".fourth");
-const analogClock = document.querySelector(".analog-clock");
+const firstClockColumn = document.querySelectorAll('.first');
+const secondClockColumn = document.querySelectorAll('.second');
+const thirdClockColumn = document.querySelectorAll('.third');
+const fourthClockColumn = document.querySelectorAll('.fourth');
+const analogClock = document.querySelector('.analog-clock');
 
-const colorActive = "#E4FF50";
-const colorInactive = "lightblue";
+const colorActive = '#E4FF50';
+const colorInactive = 'lightblue';
 
-function changeBinaryState(timePart, column){
+function changeBinaryState(timePart, column) {
 	switch (timePart) {
-		case "0":
-			for(let i = 0; i < column.length; i++) {
+		case '0':
+			for (let i = 0; i < column.length; i++) {
 				column[i].style.backgroundColor = colorInactive;
 			}
 			break;
-		case "1":
-			for (let i = 0; i < column.length; i++){
-				if(i === 3) {
+		case '1':
+			for (let i = 0; i < column.length; i++) {
+				if (i === 3) {
 					column[i].style.backgroundColor = colorActive;
 					continue;
 				}
 				column[i].style.backgroundColor = colorInactive;
 			}
 			break;
-		case "2":
-			for (let i = 0; i < column.length; i++){
-				if(i === 2) {
+		case '2':
+			for (let i = 0; i < column.length; i++) {
+				if (i === 2) {
 					column[i].style.backgroundColor = colorActive;
 					continue;
 				}
 				column[i].style.backgroundColor = colorInactive;
 			}
 			break;
-		case "3":
-			for (let i = 0; i < column.length; i++){
-				if(i === 3 || i === 2) {
+		case '3':
+			for (let i = 0; i < column.length; i++) {
+				if (i === 3 || i === 2) {
 					column[i].style.backgroundColor = colorActive;
 					continue;
 				}
 				column[i].style.backgroundColor = colorInactive;
 			}
 			break;
-		case "4":
-			for (let i = 0; i < column.length; i++){
-				if(i === 1) {
+		case '4':
+			for (let i = 0; i < column.length; i++) {
+				if (i === 1) {
 					column[i].style.backgroundColor = colorActive;
 					continue;
 				}
 				column[i].style.backgroundColor = colorInactive;
 			}
 			break;
-		case "5":
-			for (let i = 0; i < column.length; i++){
-				if(i === 1 || i  === 3) {
+		case '5':
+			for (let i = 0; i < column.length; i++) {
+				if (i === 1 || i === 3) {
 					column[i].style.backgroundColor = colorActive;
 					continue;
 				}
 				column[i].style.backgroundColor = colorInactive;
 			}
 			break;
-		case "6":
-			for (let i = 0; i < column.length; i++){
-				if(i === 1 || i === 2) {
+		case '6':
+			for (let i = 0; i < column.length; i++) {
+				if (i === 1 || i === 2) {
 					column[i].style.backgroundColor = colorActive;
 					continue;
 				}
 				column[i].style.backgroundColor = colorInactive;
 			}
 			break;
-		case "7":
-			for (let i = 0; i < column.length; i++){
-				if(i === 1 || i === 2 || i === 3) {
+		case '7':
+			for (let i = 0; i < column.length; i++) {
+				if (i === 1 || i === 2 || i === 3) {
 					column[i].style.backgroundColor = colorActive;
 					continue;
 				}
 				column[i].style.backgroundColor = colorInactive;
 			}
 			break;
-		case "8":
-			for (let i = 0; i < column.length; i++){
-				if(i === 0) {
+		case '8':
+			for (let i = 0; i < column.length; i++) {
+				if (i === 0) {
 					column[i].style.backgroundColor = colorActive;
 					continue;
 				}
 				column[i].style.backgroundColor = colorInactive;
 			}
 			break;
-		case "9":
-			for (let i = 0; i < column.length; i++){
-				if(i === 0 || i === 3) {
+		case '9':
+			for (let i = 0; i < column.length; i++) {
+				if (i === 0 || i === 3) {
 					column[i].style.backgroundColor = colorActive;
 					continue;
 				}
@@ -100,8 +100,14 @@ function changeBinaryState(timePart, column){
 
 function setBinaryTime() {
 	const time = new Date();
-	const hour = time.getHours().toString();
-	const minutes = time.getMinutes().toString();
+	let hour = (time.getUTCHours() + 2).toString();
+	const minutes = time.getUTCMinutes().toString();
+
+	if (hour === '24') {
+		hour = '00';
+	} else if (hour === '25') {
+		hour = '01';
+	}
 
 	changeBinaryState(hour[0], firstClockColumn);
 	changeBinaryState(hour[1], secondClockColumn);
@@ -109,11 +115,17 @@ function setBinaryTime() {
 	changeBinaryState(minutes[1], fourthClockColumn);
 }
 
-function setAnalogTime(){
+function setAnalogTime() {
 	const time = new Date();
-	const hour = time.getHours().toString();
+	let hour = (time.getUTCHours() + 2).toString();
 	const minutes = time.getMinutes().toString();
-	
+
+	if (hour === '24') {
+		hour = '00';
+	} else if (hour === '25') {
+		hour = '01';
+	}
+
 	analogClock.innerHTML = `${hour} : ${minutes}`;
 }
 
