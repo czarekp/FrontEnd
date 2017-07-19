07b24477295d347a66f6a5c5ddda8514c5286a74
Unknown <cezarypiotrpietrzak@gmail.com>
2017-07-19 14:34:52 +0200

    Added solutions for Module 1: get__Element & addTo...
    - getNthElement
    - getFirstElement
    - getLastElement
    - addToFront
    - addToBack

---
+++ HackReactorPrepCourseCodes/Module 1/getFirstElement.js
@@ -0,0 +1,21 @@
+/*
+Instructions:
+Write a function called "getFirstElement".
+
+Given an array, "getFirstElement" returns the first element of the given array.
+
+Notes:
+* If the given array has a length of 0, it should return undefined.
+
+var output = getFirstElement([1, 2, 3, 4, 5]);
+console.log(output); // --> 1
+
+Starter Code :
+function getFirstElement(array) {
+  // your code here
+}
+*/
+
+function getFirstElement(array) {
+	return array[0];
+}
