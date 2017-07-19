68f8e0bf9f62498e979435a0d46d7ae688d8fdb6
Unknown <cezarypiotrpietrzak@gmail.com>
2017-07-19 18:44:56 +0200

    Added 5 solutions to Module 1
    - removeElement
    - getFirstElementOfProperty
    - getNthElementOfProperty
    - getLastElementOfProperty
    - keep

---
+++ HackReactorPrepCourseCodes/Module 1/getFirstElementOfProperty.js
@@ -0,0 +1,35 @@
+/*
+Instructions:
+Write a function called "getFirstElementOfProperty".
+
+Given an object and a key, "getFirstElementOfProperty" returns the first element of the array located at the given key. 
+
+Notes:
+* If the array is empty, it should return undefined.
+* If the property at the given key is not an array, it should return undefined.
+* If there is no property at the key, it should return undefined.
+
+
+var obj = {
+  key: [1, 2, 4]
+};
+var output = getFirstElementOfProperty(obj, 'key');
+console.log(output); // --> 1
+
+Starter Code :
+function getFirstElementOfProperty(obj, key) {
+  // your code here
+}
+*/
+
+function getFirstElementOfProperty(obj, key) {
+	const isTheArrayEmpty = (obj[key] === []);
+	const isNotAnArray = !Array.isArray(obj[key]);
+	const noPropertyAtTheKey = !obj.hasOwnProperty(key);
+	
+	if(isTheArrayEmpty || isNotAnArray || noPropertyAtTheKey){
+		return undefined;
+	} else {
+		return obj[key][0];
+	}
+}
---
+++ HackReactorPrepCourseCodes/Module 1/keep.js
@@ -0,0 +1,29 @@
+/*
+Instructions:
+Write a function called "keep".
+
+Given an array and a keeper element, "keep" returns an array containing the items that match the given keeper element.
+
+Notes:
+* If no elements match, "keep" should return an empty array.
+
+var output = keep([1, 2, 3, 2, 1], 2) 
+console.log(output); --> [2, 2]
+
+Starter Code :
+function keep(array, keeper) {
+  // your code here
+}
+*/
+
+function keep(array, keeper) {
+	let newArr = [];
+	
+	for(let i = 0; i < array.length; i++){
+		if(array[i] === keeper){
+			newArr.push(keeper);
+		}
+	}
+	
+	return newArr;
+}
